6cb77a84272603a378614e0e4fb175b2be5a7709
CYJM98 <54928382+CYJM98@users.noreply.github.com>
2020-03-23 02:00:22 +0800

    Updated Product Microservice

--- microservices/products.py
+++ microservices/products.py
@@ -18,67 +18,52 @@ class Product(db.Model):
     ProductType = db.Column(db.String(100), nullable=False)
     ProductDescription = db.Column(db.String(999), nullable=False)
     Quantity = db.Column(db.Integer nullable=False)
-    UnitPr = db.Column(db.DateTime, nullable=False)
-    Gender = db.Column(db.String(1), nullable=False)
-    MobileNum = db.Column(db.Integer, nullable=False)
-    Address = db.Column(db.String(100), nullable=False)
-    UnitNum = db.Column(db.String(10), nullable=False)
-    Country = db.Column(db.String(100), nullable=False)
-    RegistrationDate = db.Column(db.DateTime, nullable=False)
-    NewsletterSubscription = db.Column(db.String(1), nullable=False)
+    UnitPrice = db.Column(db.Float, nullable=False)
+    UnitWeight = db.Column(db.Float, nullable=False)
+    ProductImage = db.Column(db.String(50), nullable=False)
 
-    def __init__(self, CustID, CustEmail, Password, FirstName, LastName, Birthdate, Gender, MobileNum, Address, UnitNum, PostalCode, Country, RegistrationDate, NewsletterSubscription):
-        self.CustID = CustID
-        self.CustEmail = CustEmail
-        self.Password = Password
-        self.FirstName = FirstName
-        self.LastName = LastName
-        self.Birthdate = Birthdate
-        self.Gender = Gender
-        self.MobileNum = MobileNum
-        self.Address = Address
-        self.UnitNum = UnitNum
-        self.Country = Country
-        self.RegistrationDate = RegistrationDate
-        self.NewsletterSubscription = NewsletterSubscription
+    def __init__(self, ProductID, ProductName, ProductType, ProductDescription, Quantity, UnitPrice, UnitWeight, ProductImage):
+        self.ProductID = ProductID
+        self.ProductName = ProductName
+        self.ProductType = ProductType
+        self.ProductDescription = ProductDescription
+        self.Quantity = Quantity
+        self.UnitPrice = UnitPrice
+        self.UnitWeight = UnitWeight
+        self.ProductImage = ProductImage
 
     def json(self):
-        return {"CustID": self.CustID, 
-                "CustEmail": self.CustEmail,
-                "Password": self.Password,
-                "FirstName": self.FirstName, 
-                "LastName": self.LastName,
-                "Birthdate": self.Birthdate,
-                "Gender": self.Gender,
-                "MobileNum": self.MobileNum,
-                "Address": self.Address,
-                "UnitNum": self.UnitNum,
-                "Country": self.Country,
-                "RegistrationDate": self.RegistrationDate,
-                "NewsletterSubscription": self.NewsletterSubscription
+        return {"ProductID": self.ProductID, 
+                "ProductName": self.ProductName,
+                "ProductType": self.ProductType,
+                "ProductDescription": self.ProductDescription, 
+                "Quantity": self.Quantity,
+                "UnitPrice": self.UnitPrice,
+                "UnitWeight": self.UnitWeight,
+                "ProductImage": self.ProductImage,
                 }
 
 
 @app.route("/product")
 def get_all():
-    return jsonify({"customers": [product.json() for product in Customer.query.all()]})
+    return jsonify({"products": [product.json() for product in Customer.query.all()]})
 
 
-@app.route("/product/<string:CustEmail>", methods=['GET'])
-def find_by_CustEmail(CustEmail):
-    product = Customer.query.filter_by(CustEmail=CustEmail).first()
+@app.route("/product/<string:ProductName>", methods=['GET'])
+def find_by_ProductName(ProductName):
+    product = Customer.query.filter_by(ProductName=ProductName).first()
     if product:
         return jsonify(product.json())
-    return jsonify({"message": "CustEmail not found."}), 404
+    return jsonify({"message": "ProductName not found."}), 404
 
 
-@app.route("/product/<string:CustEmail>", methods=['POST'])
-def create_customer(CustEmail):
-    if (Customer.query.filter_by(CustEmail=CustEmail).first()):
-        return jsonify({"message": "Customer'{}' already exists.".format(CustEmail)}), 400
+@app.route("/product/<string:ProductName>", methods=['POST'])
+def create_customer(ProductName):
+    if (Customer.query.filter_by(ProductName=ProductName).first()):
+        return jsonify({"message": "Customer'{}' already exists.".format(ProductName)}), 400
 
     data = request.get_json()
-    product = Customer(CustEmail, **data)
+    product = Customer(ProductName, **data)
 
     try:
         db.session.add(product)
@@ -87,18 +72,6 @@ def create_customer(CustEmail):
         return jsonify({"message": "An error occurred creating product."}), 500
 
     return jsonify(product.json()), 201
-
-@app.route("/product/<string:CustEmail>", methods=['PUT'])
-def update_customer(CustEmail):
-    product = Customer.query.get(CustEmail)
-    CustEmail = request.json['CustEmail']
-    Password = request.json['Password']
-
-    product.CustEmail = CustEmail
-    product.Password = Password
-
-    db.session.commit()
-    return customer_schema.jsonify(product)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
